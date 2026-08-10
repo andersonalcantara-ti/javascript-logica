0e77f05ac31843bf0529c899ad41f04983353c95
Anderson Alcantara <andersonalcantara.ti@gmail.com>
2026-08-10 09:34:41 -0300

    feat: cria arquivo para inicio de estudo de logica

---
+++ logica-estudo.js
@@ -0,0 +1,13 @@
+/*
+Pós-Graduação Senac SP
+Anderson Pedro de Alcântara
+Iniciando lógica de programação com JavaScript.
+*/
+
+console.log ("Olá Mundo!");
+
+//declarando variáveis
+let numeroUm = 5;
+let numeroDois = 10;
+console.log("Primeiro número: " + numeroUm,  "\nSegundo número: " + numeroDois);
+
