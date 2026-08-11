7ffed43fb5f0ff6c6ec3f661299a2e319ad33797
Anderson Alcantara <andersonalcantara.ti@gmail.com>
2026-08-11 20:23:59 -0300

    feat adicina o script object

--- object.js
+++ object.js
@@ -5,3 +5,13 @@ Criando objetos e mostrando atributos
 console.log("---------------");
 console.log("--- Objeto ---");
 console.log("---------------\n");
+
+const car = {
+    cor: "azul",
+    ano: 1996,
+    portas: 2,
+}
+
+console.log(car.cor);
+car.cor = "vermelho"; //muda a cor para vermelho. (Atributo cor do objeto, passa a ser vermelho)
+console.log(car.cor);
