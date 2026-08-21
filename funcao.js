25056ead1b8510adc6d39120c5f74dbe167453a9
Anderson Alcantara <andersonalcantara.ti@gmail.com>
2026-08-21 18:37:04 -0300

    feat: adiciona funcao.js
    Só estudando e explorando um pouco sobre funções.

--- funcao.js
+++ funcao.js
@@ -1,6 +1,26 @@
 //Só praticando coisas doidas :p
+/*
 function eba(){
     console.log("Bah");
 }
 
 eba()
+*/
+/*
+function eba(){
+    return console.log("Bah");
+}
+eba();
+*/
+/*
+function eba(a, b){
+    const result = (a + b);
+    console.log(result);
+}
+eba(3,4);
+*/
+
+function calcularValor(valor1, valor2){
+    return 1 + (valor1 * valor2);
+}
+console.log(calcularValor(2, 2));
